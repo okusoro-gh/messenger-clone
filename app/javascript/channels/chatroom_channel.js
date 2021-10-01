07c9e50bdeed6699370bca897dd7a65ba61f585c
KryptOdy <okusoro@gmail.com>
2021-09-30 20:05:47 -0400

    clean up messages ui

--- app/javascript/channels/chatroom_channel.js
+++ app/javascript/channels/chatroom_channel.js
@@ -1,22 +1,41 @@
 import consumer from "./consumer"
 
-consumer.subscriptions.create({ channel: "ChatroomChannel", room_id: 2 }, {
-  connected() {
-    // Called when the subscription is ready for use on the server
-    console.log("connected")
-  },
+document.addEventListener('turbolinks:load', () => {
+  const roomElement = document.getElementById('room-id');
+  const roomId = Number(roomElement.getAttribute('data-room-id'));
 
-  disconnected() {
-    // Called when the subscription has been terminated by the server
-  },
+  consumer.subscriptions.create({ channel: "ChatroomChannel", room_id: roomId }, {
+    connected() {
+      // Called when the subscription is ready for use on the server
+      console.log("connected to" + roomId)
+    },
 
-  received(data) {
-    // Called when there's incoming data on the websocket for this channel
-    const messages = document.getElementById("messages");
-    console.log(messages.innerHTML);
-    messages.innerHTML = messages.innerHTML + data.html;
-    console.log(messages.innerHTML);
-    console.log(data);
-  }
-});
+    disconnected() {
+      // Called when the subscription has been terminated by the server
+    },
+
+    received(data) {
+      // Called when there's incoming data on the websocket for this
+
+      const element = document.getElementById("user-id");
+      const user_id = Number(element.getAttribute("data-user-id"));
+
+      let html;
+
+      if (user_id === data.message.user_id) {
+        html = data.my_message
+      } else {
+        html = data.their_message;
+      }
+
+      const messages = document.getElementById("messages");
+      console.log(messages.innerHTML);
+      messages.innerHTML = messages.innerHTML + html;
+      console.log(messages.innerHTML);
+      console.log(data);
+    }
+  });
+
+})
+
 
